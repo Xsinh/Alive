bdf1a2079f979a48bf430e6ccd922dab25505c2c
Dmitriy <funnybalmosh@gmail.com>
2017-03-22 19:04:11 +0300

    Alive
    Menu v0.1

---
+++ Alive/Assets/Scripts/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour 
+{
+	public GameObject Gear;
+
+	void OnMouseDown()
+	{
+		Gear.GetComponent<Animator> ().Play ("gear_1");
+	}
+
+	void OnMouseUp()
+	{
+		Gear.GetComponent<Animator> ().Play ("gear");
+	}
+}
