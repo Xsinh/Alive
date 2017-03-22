de392058546dfa67a7e03e40a472741587ec4aee
Dmitriy <funnybalmosh@gmail.com>
2017-03-22 20:41:12 +0300

    Blood animation
    - add blood animation

---
+++ Alive/Assets/Scripts/Blood.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blood : MonoBehaviour 
+{
+	public GameObject blood;
+	float i = 0;
+	void Update ()
+	{	
+		i += 0.01f;
+		blood.gameObject.transform.localScale = new Vector3 (i,i,0);
+	}
+}
